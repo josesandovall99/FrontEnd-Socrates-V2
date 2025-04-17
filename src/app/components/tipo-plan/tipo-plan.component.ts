90da60e4a7f320f943d2c52527d5f9a020eb1276
Edgar Parada <edgaryamillparadaduarte@gmail.com>
2025-04-17 09:43:26 -0500

    Cree carpetas necesarias y mis componentes

---
+++ src/app/components/tipo-plan/tipo-plan.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tipo-plan',
+  standalone: true,
+  imports: [],
+  templateUrl: './tipo-plan.component.html',
+  styleUrl: './tipo-plan.component.css'
+})
+export class TipoPlanComponent {
+
+}
