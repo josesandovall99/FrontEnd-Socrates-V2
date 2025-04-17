2614181c005810e0d0237e8303c6f3b25ec5ff41
Edgar Parada <edgaryamillparadaduarte@gmail.com>
2025-04-17 10:57:31 -0500

    Cree componente de servicio

--- src/app/components/servicio/servicio.component.ts
+++ src/app/components/servicio/servicio.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-servicio',
-  standalone: true,
-  imports: [],
+  standalone: true,  // <-- Importante para componentes independientes
   templateUrl: './servicio.component.html',
-  styleUrl: './servicio.component.css'
+  styleUrls: ['./servicio.component.css'],
+  imports: [ReactiveFormsModule] // <-- ¡Importa ReactiveFormsModule aquí!
 })
 export class ServicioComponent {
+  servicioForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.servicioForm = this.fb.group({
+      fechaServicio: ['', Validators.required],
+      descripcion: ['', Validators.required],
+      horaServicio: ['', Validators.required],
+      estado: ['', Validators.required],
+      tipoPlan: ['', Validators.required],
+      tecnico: [''],
+      fechaRegistro: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.servicioForm.valid) {
+      console.log('Servicio registrado:', this.servicioForm.value);
+    }
+  }
 }
